f676f701cdf4d3e6f9a3529321c4d4eca91b98ca
micharambou <michael.rambousek@googlemail.com>
2024-04-15 16:04:33 +0200

    - new behaviour: on group_view activation: instead of loading a collection filter preset, append current collection filter by filter tag - README.md: remove 'Create collection preset' section in setup instructions (not required anymore due to new collection filter behaviour)

--- README.md
+++ README.md
@@ -3,20 +3,19 @@ darktable group-view
 This adds a shortcut and a "Toogle group-view" button in darktable's lighttable selection module.
 In group-view mode, only images that are grouped together are shown.
 
-To achieve this, a temporary tag "group-view" becomes attached to affected images and a collection module preset of the same name becomes activated. Once `group-view` mode is left, all attached `group-view` tags become cleaned up.
+To achieve this, a temporary tag "group-view" becomes attached to affected images and the current collection filter expanded by this filter tag. Once `group-view` mode is left, all attached `group-view` tags become cleaned up.
 
 Installation
 ------------
 ### Using script_manager
 
 * open `action` `install/update scripts`
-* in the URL box enter `https://github.com/micharambou/darktable-group-view.git`
+* in the URL box enter `https://github.com/micharambou/:w
+darktable-group-view.git`
 * in the category box enter `dt-group-view`
 * click the `Install additional scripts` button
   
 ### Create collection preset `group-view`
-
-* In Collection module add a new collection preset and name it `group-view`. Select `tag` as filter criteria. You may have to create the corresponding tag once by yourself to be able to select it as a value for your preset.
 
 Usage
 -----
--- group-view.lua
+++ group-view.lua
@@ -6,95 +6,115 @@ local MODULE = "group-view"
 du.check_min_api_version("9.1.0", MODULE)
 
 if dt.tags.find("group-view") == nil then
-    dt.tags.create("group-view")
+	dt.tags.create("group-view")
 end
 local filter_tag = dt.tags.find("group-view")
 
 local group_view_active = false
+local previous = nil
+
+local filter_rule = dt.gui.libs.collect.new_rule()
+filter_rule.mode = "DT_LIB_COLLECT_MODE_AND"
+filter_rule.data = "group-view"
+filter_rule.item = "DT_COLLECTION_PROP_TAG"
 
 local function tablelength(t)
-    local count = 0
-    for _ in pairs(t) do count = count + 1 end
-    return count
+	local count = 0
+	for _ in pairs(t) do
+		count = count + 1
+	end
+	return count
 end
 
 local function cleanup()
-    for _, image in pairs(dt.tags.get_tagged_images(filter_tag)) do
-        dt.tags.detach(filter_tag, image)
-    end
+	for _, image in pairs(dt.tags.get_tagged_images(filter_tag)) do
+		dt.tags.detach(filter_tag, image)
+	end
 end
 
 cleanup()
 
+local function append_filter_rule_to_collection()
+	previous = dt.gui.libs.collect.filter()
+	local current = {}
+	for _, prev_rule in pairs(previous) do
+		table.insert(current, prev_rule)
+	end
+	table.insert(current, filter_rule)
+	dt.gui.libs.collect.filter(current)
+end
+
 local selected_images = {}
 
 local function toogle_group_view(_, _)
-    if not group_view_active then
-        group_view_active = true
-        local image = table.unpack(dt.gui.selection())
-        local t_image_group = image:get_group_members()
-        for _, image in pairs(t_image_group) do
-            dt.tags.attach(filter_tag, image)
-        end
-        dt.gui.action("lib/collect/preset/group-view", 1, 000)
-        return { image }
-    else
-        group_view_active = false
-        dt.gui.action("lib/collect/jump back to previous collection", 1, 000)
-        cleanup()
-        return selected_images
-    end
+	if not group_view_active then
+		group_view_active = true
+		local image = table.unpack(dt.gui.selection())
+		local t_image_group = image:get_group_members()
+		for _, image in pairs(t_image_group) do
+			dt.tags.attach(filter_tag, image)
+		end
+		-- dt.gui.action("lib/collect/preset/group-view", 1, 000)
+        append_filter_rule_to_collection()
+		return { image }
+	else
+		group_view_active = false
+		dt.gui.action("lib/collect/jump back to previous collection", 1, 000)
+		cleanup()
+		return selected_images
+	end
 end
 
 local function destroy()
-    cleanup()
-    dt.gui.libs.select.destroy_selection(MODULE)
-    dt.destroy_event("group-view: image selection changed", "selection-changed")
+	cleanup()
+	dt.gui.libs.select.destroy_selection(MODULE)
+	dt.destroy_event("group-view: image selection changed", "selection-changed")
 end
 
 dt.gui.libs.select.register_selection(
-    MODULE,
-    "Toogle group-view",
-    toogle_group_view,
-    "toogle group view to show only group members of currently selected image")
+	MODULE,
+	"Toogle group-view",
+	toogle_group_view,
+	"toogle group view to show only group members of currently selected image"
+)
 
 dt.gui.libs.select.set_sensitive(MODULE, false)
 
 dt.register_event("group-view: image selection changed", "selection-changed", function(_)
-    if (next(dt.gui.selection()) == nil and not group_view_active) then
-        dt.gui.libs.select.set_sensitive(MODULE, false)
-        selected_images = nil
-        cleanup()
-    end
-    if tablelength(dt.gui.selection()) == 1 then
-        dt.gui.libs.select.set_sensitive(MODULE, true)
-        selected_images = dt.gui.selection()
-    end
-    if (tablelength(dt.gui.selection()) > 1 and not group_view_active) then
-        dt.gui.libs.select.set_sensitive(MODULE, false)
-        selected_images = dt.gui.selection()
-    end
+	if next(dt.gui.selection()) == nil and not group_view_active then
+		dt.gui.libs.select.set_sensitive(MODULE, false)
+		selected_images = nil
+		cleanup()
+	end
+	if tablelength(dt.gui.selection()) == 1 then
+		dt.gui.libs.select.set_sensitive(MODULE, true)
+		selected_images = dt.gui.selection()
+	end
+	if tablelength(dt.gui.selection()) > 1 and not group_view_active then
+		dt.gui.libs.select.set_sensitive(MODULE, false)
+		selected_images = dt.gui.selection()
+	end
 end)
 
 dt.register_event("toogle group view", "shortcut", function()
-    if (next(dt.gui.selection()) == nil and not group_view_active) then
-        dt.gui.libs.select.set_sensitive(MODULE, false)
-        selected_images = nil
-        cleanup()
-    end
-    if (next(dt.gui.selection()) == nil and group_view_active) then
-        selected_images = nil
-        toogle_group_view()
-    end
-    if tablelength(dt.gui.selection()) == 1 then
-        dt.gui.libs.select.set_sensitive(MODULE, true)
-        selected_images = dt.gui.selection()
-        toogle_group_view()
-    end
-    if (tablelength(dt.gui.selection()) > 1 and not group_view_active) then
-        dt.gui.libs.select.set_sensitive(MODULE, false)
-        selected_images = dt.gui.selection()
-    end
+	if next(dt.gui.selection()) == nil and not group_view_active then
+		dt.gui.libs.select.set_sensitive(MODULE, false)
+		selected_images = nil
+		cleanup()
+	end
+	if next(dt.gui.selection()) == nil and group_view_active then
+		selected_images = nil
+		toogle_group_view()
+	end
+	if tablelength(dt.gui.selection()) == 1 then
+		dt.gui.libs.select.set_sensitive(MODULE, true)
+		selected_images = dt.gui.selection()
+		toogle_group_view()
+	end
+	if tablelength(dt.gui.selection()) > 1 and not group_view_active then
+		dt.gui.libs.select.set_sensitive(MODULE, false)
+		selected_images = dt.gui.selection()
+	end
 end, "toogle group view")
 
 local script_data = {}
